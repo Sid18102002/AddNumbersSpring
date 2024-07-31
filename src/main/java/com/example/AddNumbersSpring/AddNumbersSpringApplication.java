6ba815f607758562cf2b46f214ba58635d377896
Sid18102002 <zawarsiddhant@gmail.com>
2024-08-01 02:09:35 +0530

    first

---
+++ src/main/java/com/example/AddNumbersSpring/AddNumbersSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.AddNumbersSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddNumbersSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddNumbersSpringApplication.class, args);
+	}
+}
